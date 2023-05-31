76f7819c641fdd608a73588c73ce90d19504aeb0
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2023-05-31 17:14:21 +0000

    feat(login): updated style

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,24 +1,59 @@
 import * as React from 'react'
+import './auth.css'
 
 type PropsType = {
     loginUseState: boolean,
     setLoginUseState: React.Dispatch<React.SetStateAction<boolean>>
 }
+function handleLogin() {
+    // do something
+}
 
 const login = (props: PropsType) => {
     return (
-        <div className="flex mb-2 center bg-white max-width-400">
-            <div className="">
-                <h1 className="mb-2 text-2xl font-extrabold leading-none tracking-tight text-black-700 md:text-3xl lg:text-3xl dark:text-white"> 
-                    Sign in to your account 
-                </h1>
-                <p className="mb-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-                    Continue with Google or enter your details
-                </p>
-                <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Learn more
-                    <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </button>
+        <div className="flex wrap max-width-350 bg-white py-4 px-8 my-4 rounded-lg">
+            <h1 className="mb-3 text-2xl font-extrabold leading-none tracking-tight text-black-700"> 
+                Sign in to your account 
+            </h1>
+
+            <p className="mb-4 text-lg font-small text-gray-500 lg:text-xl dark:text-gray-400">
+                Continue with Google or enter your details
+            </p>
+
+            <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-black bg-white-700 rounded-lg lg-google-btn">
+                Log in with Google
+            </button>
+
+            <div className="seperator flex py-4 justify-self-center justify-between items-center gap-x-3 self-center">
+                <hr className="w-28" /> or <hr className="w-28" />
+            </div>
+
+            <div className="form-control">
+                <form onSubmit={handleLogin} className="flex flex-col">
+                    <div className="flex flex-col">
+                        <input 
+                            type="text" 
+                            className="w-full lgn-fields" 
+                            placeholder="Email" />
+                        <input 
+                            type="password" 
+                            className="w-full lgn-fields"
+                            placeholder="Password" />
+                        <a href="/auth/forgot" className="self-end text-sm underline underline-offset-3 py-1"> Forgot Password </a>
+                    </div>
+                    <div>
+                        <button 
+                            type="submit"
+                            className="login-button flex bg-black items-center justify-center px-5 py-3 text-base font-medium text-center bg-white-700">
+                            Login 
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <div className="text-sm py-4 px-0.5">
+                Don&apos;t have an account?
+                <a href="#" className="font-bold"> Signup for free </a>
             </div>
         </div>
     )
