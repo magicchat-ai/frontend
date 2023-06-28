1abc037f68a3f358aae10f73aa171fdf8e40eefb
ayush anand <36472216+ayushanand18@users.noreply.github.com>
2023-06-28 23:41:36 +0530

    feat(UI): removes button Talk Now and replaces with Click on Card

--- app/dashboard/product_card.tsx
+++ app/dashboard/product_card.tsx
@@ -15,16 +15,17 @@ type PropsType =  {
 
 const ProductCard = (props: PropsType) => {
     return (
-        <div className="flex cursor-pointer flex-col max-w-64 px-4 py-4 gap-y-2 hover:shadow-lg hover:shadow-slate-400 rounded-lg ">
-            <div className="flex max-w-max mb-4 max-w-[250px] h-[300px]">
-                <Image alt={props.name} width="240" height="300" src={props.image_url} />
+        <div 
+            onClick={() => props.setModal(props)}
+            className="flex cursor-pointer flex-col max-w-64 px-4 py-4 gap-y-2 hover:bg-slate-100 rounded-md ">
+            <div className="flex max-w-max mb-4 max-w-[240px] h-[280px]">
+                <Image alt={props.name} width="270" height="200" src={props.image_url} />
             </div>
             <div className="flex flex-row justify-between">
                 <span className="flex text-black font-bold">{props.name}</span>
                 <span className="flex text-black font-bold">${props.price}</span>
             </div>
             <div className="flex flex-col text-slate-600 dark:text-slate-600 text-sm">{props.tagline}</div>
-            <button onClick={() => props.setModal(props)} className="flex flex-row rounded-full border-[#1e1e1e] border max-w-max mt-4 py-2 px-6 font-bold text-[#1e1e1e]">Talk now</button>
         </div>
     )
 }
